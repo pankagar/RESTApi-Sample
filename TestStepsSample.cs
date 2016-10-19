0f786decf0baf207609102907b7232e8d390ec68
pankagar13 <pankagar@microsoft.com>
2016-10-19 16:05:49 +0530

    indent changes

--- TestStepsSample.cs
+++ TestStepsSample.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -16,25 +16,24 @@ namespace ConsoleApplication
         static void Main(string[] args)
         {
 
-			// provide collection url of tfs
+            // provide collection url of tfs
             var collectionUri = "http://pankagar-hp2:8080/tfs/DefaultCollection";
             VssCredentials connection = new VssCredentials(true);
             var testPlanId = 1;
             var testSuiteId = 2;
             var projectName = "agile";
 
-			
-			// create workitemtracking client
+            // create workitemtracking client
             var _witClient = new WorkItemTrackingHttpClient(new Uri(collectionUri), connection);
 
             // upload attachment
             FileStream uploadStream = File.Open(@"C: \Users\pankagar\Downloads\Canvas.png", FileMode.Open, FileAccess.Read);
             var attachmentObject = _witClient.CreateAttachmentAsync(uploadStream, "Canvas.png", "Simple").Result;
 
-			// create a patchdocument object
-			JsonPatchDocument json = new JsonPatchDocument();
+            // create a patchdocument object
+            JsonPatchDocument json = new JsonPatchDocument();
             
-			// create a new patch operation for title field
+            // create a new patch operation for title field
             JsonPatchOperation patchDocument1 = new JsonPatchOperation();
             patchDocument1.Operation = Operation.Add;
             patchDocument1.Path = "/fields/System.Title";
@@ -49,12 +48,12 @@ namespace ConsoleApplication
             json.Add(patchDocument2);
 
                       
-			// create testbasehelper object
+            // create testbasehelper object
             TestBaseHelper helper = new TestBaseHelper();
-			// create testbase object to utilize teststep helpers
+            // create testbase object to utilize teststep helpers
             ITestBase tb = helper.Create();
-			
-			 // create 2 test steps ts1 and ts2
+
+            // create 2 test steps ts1 and ts2
             ITestStep ts1 = tb.CreateTestStep();
             ITestStep ts2 = tb.CreateTestStep();
             ts1.Title = "title -> title1";
@@ -64,16 +63,16 @@ namespace ConsoleApplication
             ts1.Description = "description1";
             ts2.Description = "description2";
             // adding attachment to step1
-			ts1.Attachments.Add(ts1.CreateAttachment(attachmentObject.Url, "CanvasImage"));
+             ts1.Attachments.Add(ts1.CreateAttachment(attachmentObject.Url, "CanvasImage"));
 
-			// add your steps actions to testbase object
+            // add your steps actions to testbase object
             tb.Actions.Add(ts1);
             tb.Actions.Add(ts2);
 
-			// update json based on all actions (including teststeps and teststep attachemnts) 
+            // update json based on all actions (including teststeps and teststep attachemnts) 
             json = tb.SaveActions(json);
 
-			var xml = "";
+            var xml = "";
             /* getting xml for teststeps
             xml = tb.GenerateXmlFromActions();
             */
@@ -92,7 +91,7 @@ namespace ConsoleApplication
                 xml = testCaseObject.Fields["Microsoft.VSTS.TCM.Steps"].ToString();
                 tb = helper.Create();
 
-				// create tcmattachemntlink object from workitem relation, teststep helper will use this
+                // create tcmattachemntlink object from workitem relation, teststep helper will use this
                 IList<TestAttachmentLink> tcmlinks= new List<TestAttachmentLink>();
                 foreach (WorkItemRelation rel in testCaseObject.Relations)
                 {
@@ -103,11 +102,11 @@ namespace ConsoleApplication
                     tcmlinks.Add(tcmlink);
                     
                 }
-				
-				// load teststep xml and attachemnt links
+
+                // load teststep xml and attachemnt links
                 tb.LoadActions(xml, tcmlinks);
                 
-				ITestStep ts;
+                ITestStep ts;
                 //updating 1st test step
                 ts = (ITestStep)tb.Actions[0];
                 ts.Title = "title -> title11";
@@ -124,14 +123,14 @@ namespace ConsoleApplication
 
                 JsonPatchDocument json2 = new JsonPatchDocument();
                 // update json based on all new changes ( updated step xml and attachments)
-				json2 = tb.SaveActions(json2);
+                json2 = tb.SaveActions(json2);
 
-				// update testcase wit using new json
+                // update testcase wit using new json
                 testCaseObject = _witClient.UpdateWorkItemAsync(json2, testCaseId).Result;
-				
-				
-				/* Note : If you want to remove attachment then create new patchOperation, details are available here : https://www.visualstudio.com/en-us/docs/integrate/api/wit/work-items#remove-an-attachment				
-				*/
+
+                /* Note : If you want to remove attachment then create new patchOperation, details are available here :
+                          https://www.visualstudio.com/en-us/docs/integrate/api/wit/work-items#remove-an-attachment
+                */
             }
         }
     }
--- TestStepsSample.js
+++ TestStepsSample.js
@@ -4,91 +4,90 @@
 "use strict";
 
 VSS.init({
-		  usePlatformScripts: true, 
-		  usePlatformStyles: true
-		});
-	  
-		VSS.ready(function() {
-		  // Start using VSS
-		
+ usePlatformScripts: true,
+ usePlatformStyles: true
+});
 
-		var apis = ["VSS/Controls", "VSS/Service", "TFS/TestManagement/RestClient", "TFS/TestManagement/Contracts", "TFS/TestManagement/Utils", "TFS/WorkItemTracking/RestClient", "TFS/WorkItemTracking/Contracts", "VSS/WebApi/Contracts"];
+VSS.ready(function() {
+// Start using VSS
 
-		VSS.require(apis, (Controls, VSS_Service, TFS_Test_WebApi, TestContracts, Utils, WorkItemRestClient, WitContract, VSS_Common_Contracts) => {
-						
-			// title
-			var patchDocument1 = {
-				from: null,
-				op: VSS_Common_Contracts.Operation.Add,
-				path: "/fields/System.Title",
-				value: "Testing Rest Api"
-			};
-		
-			//priority
-			var patchDocument2 = {
-				from: null,
-				op: VSS_Common_Contracts.Operation.Add,
-				path: "/fields/Microsoft.VSTS.Common.Priority",
-				value: "2"
-			};
-			
-			// create testbasehelper object and then create testbase object to utilize teststep helpers
-			var helper = new Utils.TestBaseHelper();
-            var tb = helper.create();
-			
-			// create 2 test steps step1 and step2
-            var step1 = tb.createTestStep();
-            var step2 = tb.createTestStep();
-            step1.setTitle("action1");
-            step2.setTitle("title ->  title1");
-            step1.setExpectedResult("expect1");
-            step2.setExpectedResult("expected2");
-            step1.description = "desc1";
-            step2.description = "desc2";
-			step1.stepType = Utils.TestStepType.Validate;
-			
-			// attachment objects got from createAttachment call
-            var attachment1 = "http://pankagar-hp2:8080/tfs/DefaultCollection/_apis/wit/attachments/955e7a7d-a6c4-4af6-bfe1-e909bd93db63?fileName=Canvas%20Image";
-            var attachment2 = "http://pankagar-hp2:8080/tfs/DefaultCollection/_apis/wit/attachments/ddcc418c-dffa-4008-b3db-486395dccc80?fileName=SystemInformation";
-            var attachment3 = "http://pankagar-hp2:8080/tfs/DefaultCollection/_apis/wit/attachments/ce7e0a01-46da-40fb-a957-2ece3aae7589?fileName=Nuget%20Package";
-            var attachment4 = "http://pankagar-hp2:8080/tfs/DefaultCollection/_apis/wit/attachments/dbfbac3f-3d8a-4dda-b2d9-5990bdb67671?fileName=Xt%20Session%20report";
-            var attachment5 = "http://pankagar-hp2:8080/tfs/DefaultCollection/_apis/wit/attachments/4be2ee2e-e3ee-4771-86c9-ef349da60d82?fileName=screen%20shot";
+    var apis = ["VSS/Controls", "VSS/Service", "TFS/TestManagement/RestClient", "TFS/TestManagement/Contracts", "TFS/TestManagement/Utils", "TFS/WorkItemTracking/RestClient", "TFS/WorkItemTracking/Contracts", "VSS/WebApi/Contracts"];
+
+    VSS.require(apis, (Controls, VSS_Service, TFS_Test_WebApi, TestContracts, Utils, WorkItemRestClient, WitContract, VSS_Common_Contracts) => {
+
+        // title
+        var patchDocument1 = {
+            from: null,
+            op: VSS_Common_Contracts.Operation.Add,
+            path: "/fields/System.Title",
+            value: "Testing Rest Api"
+        };
+
+        //priority
+        var patchDocument2 = {
+            from: null,
+            op: VSS_Common_Contracts.Operation.Add,
+            path: "/fields/Microsoft.VSTS.Common.Priority",
+            value: "2"
+        };
+
+        // create testbasehelper object and then create testbase object to utilize teststep helpers
+        var helper = new Utils.TestBaseHelper();
+        var tb = helper.create();
+
+        // create 2 test steps step1 and step2
+        var step1 = tb.createTestStep();
+        var step2 = tb.createTestStep();
+        step1.setTitle("action1");
+        step2.setTitle("title ->  title1");
+        step1.setExpectedResult("expect1");
+        step2.setExpectedResult("expected2");
+        step1.description = "desc1";
+        step2.description = "desc2";
+        step1.stepType = Utils.TestStepType.Validate;
+
+        // attachment objects got from createAttachment call
+        var attachment1 = "http://pankagar-hp2:8080/tfs/DefaultCollection/_apis/wit/attachments/955e7a7d-a6c4-4af6-bfe1-e909bd93db63?fileName=Canvas%20Image";
+        var attachment2 = "http://pankagar-hp2:8080/tfs/DefaultCollection/_apis/wit/attachments/ddcc418c-dffa-4008-b3db-486395dccc80?fileName=SystemInformation";
+        var attachment3 = "http://pankagar-hp2:8080/tfs/DefaultCollection/_apis/wit/attachments/ce7e0a01-46da-40fb-a957-2ece3aae7589?fileName=Nuget%20Package";
+        var attachment4 = "http://pankagar-hp2:8080/tfs/DefaultCollection/_apis/wit/attachments/dbfbac3f-3d8a-4dda-b2d9-5990bdb67671?fileName=Xt%20Session%20report";
+        var attachment5 = "http://pankagar-hp2:8080/tfs/DefaultCollection/_apis/wit/attachments/4be2ee2e-e3ee-4771-86c9-ef349da60d82?fileName=screen%20shot";
             
-			// adding attachment to steps
-			step1.attachments.push(step1.createAttachment(attachment1, "canvas image"));
-            step1.attachments.push(step1.createAttachment(attachment2, "System info"));
-            step2.attachments.push(step2.createAttachment(attachment3, "nuget package"));
+        // adding attachment to steps
+        step1.attachments.push(step1.createAttachment(attachment1, "canvas image"));
+        step1.attachments.push(step1.createAttachment(attachment2, "System info"));
+        step2.attachments.push(step2.createAttachment(attachment3, "nuget package"));
             
-			// add your steps actions to tesebase object
-			tb.actions.push(step1);
-            tb.actions.push(step2);
-			
-            /* getting xml for teststeps
-            var xml = tb.generateXmlFromActions();
-            */
-				
-			var json = [patchDocument1, patchDocument2];
-			
-			// update json based on all actions (including teststeps and teststep attachemnts) 
-			json = tb.saveActions(json);
-			
-			// create workitemtracking client
-			var witClient = VSS_Service.getClient(WorkItemRestClient.WorkItemTrackingHttpClient3);
-			
-			// create Test Case
-			witClient.createWorkItem(json, VSS.getWebContext().project.name, "Test Case").then(function(workitem) {
-				console.log("create workitem id is " + workitem.id);
-				
-				// get Test Case using all relations
-				witClient.getWorkItem(workitem.id, null, null, WitContract.WorkItemExpand.Relations).then(function(workitem)
-				{
-					console.log("get workitem id is " + workitem.id);
-					
-					var xml = workitem.fields["Microsoft.VSTS.TCM.Steps"].toString();
-					
-					//create tcmattachemntlink object from workitem relation, teststep helper will use this
-					var attachmentLinks = [];
-					for (var i = 0; i < workitem.relations.length; i++) {
+        // add your steps actions to tesebase object
+        tb.actions.push(step1);
+        tb.actions.push(step2);
+
+        /* getting xml for teststeps
+        var xml = tb.generateXmlFromActions();
+        */
+
+        var json = [patchDocument1, patchDocument2];
+
+        // update json based on all actions (including teststeps and teststep attachemnts) 
+        json = tb.saveActions(json);
+
+        // create workitemtracking client
+        var witClient = VSS_Service.getClient(WorkItemRestClient.WorkItemTrackingHttpClient3);
+
+        // create Test Case
+        witClient.createWorkItem(json, VSS.getWebContext().project.name, "Test Case").then(function(workitem) {
+            console.log("create workitem id is " + workitem.id);
+
+            // get Test Case using all relations
+            witClient.getWorkItem(workitem.id, null, null, WitContract.WorkItemExpand.Relations).then(function(workitem)
+            {
+                console.log("get workitem id is " + workitem.id);
+
+                var xml = workitem.fields["Microsoft.VSTS.TCM.Steps"].toString();
+
+                //create tcmattachemntlink object from workitem relation, teststep helper will use this
+                var attachmentLinks = [];
+                for (var i = 0; i < workitem.relations.length; i++) {
                     var rel = workitem.relations[i];
                     var attachmentLink = {
                         rel: rel.rel,
@@ -97,15 +96,15 @@ VSS.init({
                         title: ""
                     };
                     attachmentLinks.push(attachmentLink);
-					}
-					
+                }
+
                 var helper2 = new Utils.TestBaseHelper();
                 var tb2 = helper2.create();
-				
-				// load teststep xml and attachemnt links
+
+                // load teststep xml and attachemnt links
                 tb2.loadActions(xml, attachmentLinks);
-				
-				// updating 1st test step, removing 2nd test step and adding new 3rd teststep
+
+                // updating 1st test step, removing 2nd test step and adding new 3rd teststep
                 var ts1 = tb2.actions[0];
                 var ts2 = tb2.actions[1];
                 ts1.setTitle("action11");
@@ -118,25 +117,24 @@ VSS.init({
                 ts3.setTitle("action3");
                 ts3.attachments.push(ts3.createAttachment(attachment5, "screenshot"));
                 tb2.actions.push(ts3);
-				
-				// update json based on all new changes ( updated step xml and attachments)
-				json= [];
-				json = tb2.saveActions(json);
-							
-					
-				// updating work item with new json changes
-				witClient.updateWorkItem(json, workitem.id).then(function (workitem) {
-                    console.log("update workitem id is " + workitem.id);
+
+                // update json based on all new changes ( updated step xml and attachments)
+                json= [];
+                json = tb2.saveActions(json);
+
+
+                // updating work item with new json changes
+                witClient.updateWorkItem(json, workitem.id).then(function (workitem) {
+                                    console.log("update workitem id is " + workitem.id);
                 });
-				
-				/* Note : If you want to remove attachment then create new patchdocument, details are available here : https://www.visualstudio.com/en-us/docs/integrate/api/wit/work-items#remove-an-attachment				
-				*/
-				
-				});
-			});
-			
-	});
-});	 
-	  
+
+                /* Note : If you want to remove attachment then create new patchdocument, details are available here : 
+                https://www.visualstudio.com/en-us/docs/integrate/api/wit/work-items#remove-an-attachment
+                */
+
+            });
+        });
+    });
+});
 
 //# sourceMappingURL=app.js.map
